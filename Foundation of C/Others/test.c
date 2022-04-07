5ef7db7e05cb6c7e5b3f5403da017b4dbc792b10
jackandrea54 <piddfae@gmail.com>
2022-04-07 14:18:46 +0700

    cm

--- Foundation of C/Others/test.c
+++ Foundation of C/Others/test.c
@@ -1,10 +1,69 @@
-#include <stdio.h>
-#include <string.h>
-int main()
+#include<stdio.h>
+#include <math.h>
+
+
+void swap(int *xp, int *yp)
 {
-	char *p = 0;
-	*p = 'a';
-	printf("%c",*p);
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void bubbleSort(int num[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)    
+    for (j = 0; j < n-i-1; j++)
+        if (num[j] > num[j+1])
+            swap(&num[j], &num[j+1]);
+}
+
+int main(){
+	int i,j,k,n,chenhlechMin,idealNum=0;
+	float ideal=0;
+	int to1,to2;
+	scanf("%d",&n);
+	int num[n];	
+	for(i=0;i<n;i++){
+		scanf("%d",&num[i]);
+		ideal+=num[i];
+	}
+		
+//	Sap xep lai
+	bubbleSort(num,n);
+//	printf("Day so la: ");
+//	for(i=0;i<n;i++){
+//		printf("%d ",num[i]);
+//	}
+	ideal /= 2;
 	
-   return(0);
+	for(i=0;i<n;i++){
+		if(ideal >= num[n-1]){
+			idealNum = n-1;
+			break;
+		}
+		if(fabs(num[i] - ideal) < fabs(num[idealNum] - ideal)){
+			idealNum = i;
+		}
+	}
+//	printf("%d",num[idealNum]);
+	
+	for(i=0;i<n-1;i++){
+		to1 = num[idealNum];
+		to2 = 0;
+		for(j = i-1; j >= 0 && j != idealNum; j--)
+			to1 += num[j];
+		for(k = i; k < n && k != idealNum; k++)
+			to2 += num[k];
+		if(i==0)
+			chenhlechMin = abs(to1-to2);
+		else if(chenhlechMin > abs(to1-to2)){
+//			printf("To1: %d To2: %d\n",to1,to2);
+			chenhlechMin = abs(to1-to2);
+		}
+			
+	}
+
+	printf("%d",chenhlechMin);
+	return 0;
 }
